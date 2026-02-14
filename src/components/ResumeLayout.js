a894d1bfe96e5948721788713f6bff2e033e236d
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-14 23:51:59 +0330

    feat: Add ResumeLayout component.

---
+++ src/components/ResumeLayout.js
@@ -0,0 +1,9 @@
+function ResumeLayout() {
+  return (
+    <div className="grid lg:grid-cols-[65%_35%] gap-5 print:grid-cols-[65%_35%]">
+      <div className="space-y-3"></div>
+    </div>
+  );
+}
+
+export default ResumeLayout;
