ed8727f53fbd6dfcb80bc472a7a389a50e34bccc
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-14 23:51:37 +0330

    feat: Initialize resume page with layout component, icon constants, and profile image.

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,13 @@
+import Header from '@/components/Header';
+import ResumeLayout from '@/components/ResumeLayout';
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="min-h-screen bg-white print:bg-white">
+      <div className="max-w-5xl mx-auto p-5 print:p-5 flex flex-col gap-3">
+        <Header />
+        <ResumeLayout />
+      </div>
+    </div>
+  );
 }
