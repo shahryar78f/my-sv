5ba87c5c215673902e1a28105e8a69eb6dd36373
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-15 17:29:39 +0330

    feat: Add `ResumeLayout` component and integrate `SummarySection` for displaying resume summary.

--- src/components/ResumeLayout.js
+++ src/components/ResumeLayout.js
@@ -1,7 +1,11 @@
+import SummarySection from "./SummarySection";
+
 function ResumeLayout() {
   return (
     <div className="grid lg:grid-cols-[65%_35%] gap-5 print:grid-cols-[65%_35%]">
-      <div className="space-y-3"></div>
+      <div className="space-y-3">
+        <SummarySection/>
+      </div>
     </div>
   );
 }
--- src/components/SummarySection.js
+++ src/components/SummarySection.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import { summary } from "@/constants"
+import Title from "./Title"
 
 function SummarySection() {
   return (
-    <div>SummarySection</div>
+   <section className="space-y-1">
+      <Title>Summary</Title>
+      <p className="text-gray-700 leading-relaxed text-xs print:text-[10px] font-bold">
+        {summary.summaryEN}
+      </p>
+    </section>
   )
 }
 
