bd2fffd379e9ea50a2d0d85e5f2ab8f5be46a78e
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-15 23:47:32 +0330

    feat: Implement initial resume layout with core components for header, summary, and experience sections.

--- src/constant/index.js
+++ src/constant/index.js
@@ -33,3 +33,21 @@ export const summary = {
   summaryEN:
     '    Frontend Developer with 2 years of hands-on experience in JavaScript, HTML, CSS, Tailwind CSS, React, and Next.js I started my journey through an intensive coding bootcamp and later collaborated with the Saftian team on a project basis, working on two e-commerce applications. Passionate about building high-performance, scalable applications with clean code and well-crafted UI/UX. Experienced in leveraging AI tools to improve development speed and code quality.',
 };
+
+export const experience = [
+  {
+    id: 1,
+    priority: 1,
+    title: 'Front-End Developer',
+    company: 'Codenight',
+    date: 'Jan 2024 - Present',
+    bullets: [
+      'Contributed to large-scale web applications, working on both client-facing pages and complex admin panels',
+      'Built high-performance, responsive user interfaces using React, Next.js, Material UI, and Tailwind CSS',
+      'Managed complex form states with Redux Toolkit and synchronized API interactions using React Query',
+      'Implemented robust form validation and data transfer objects (DTOs) to ensure backend consistency',
+      'Integrated charts, icons, and visual components to enhance usability and user experience',
+      ' Collaborated closely with cross-functional teams to deliver scalable, maintainable frontend solutions',
+    ],
+  },
+];
